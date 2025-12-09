25583a3d98714a52397884d2e289b65146c0d8de
A1rbus330 <jonahrabinovitch11@gmail.com>
2025-12-09 09:41:28 -0500

    Done

--- src/ArcadeGameCredit.java
+++ src/ArcadeGameCredit.java
@@ -1,3 +1,35 @@
 public class ArcadeGameCredit {
-    
+private String gameName;   // e.g. "Dance Machine"
+private int credits;      // how many credits are currently loaded
+private int costPerPlay;      // how many credits one play costs
+private int playsCompleted;     // used at higher levels 
+public ArcadeGameCredit(String givenGameName, int initialCredits, int initialCostPerPlay){
+    gameName = givenGameName;
+    if(initialCredits < 0){
+        credits = 0;
+    }
+    else{
+    credits = initialCredits;
+    }
+    if (initialCostPerPlay <= 0){
+        costPerPlay = 1;
+    }
+    else{
+    costPerPlay = initialCostPerPlay;
+    }
+    playsCompleted = 0;
+}
+public String toString(){
+    return "Game: " + gameName + ", Credits: " + credits + ", Cost per play: " + costPerPlay;
+}
+public void playOnce(){
+    if (credits >= costPerPlay){
+        credits = credits - costPerPlay;
+        playsCompleted++;
+    }
+}
+public int getTotalCreditsUsed(){
+    return (playsCompleted * costPerPlay);
+}
+
 }
